c6e23a3b72ba953b7eafab1ab6dba68e4382eed9
Your Name <you@example.com>
2018-07-22 16:10:58 +0800

    添加点击进入其他页面事件

--- app/src/main/java/com/example/word21_notification/MainActivity.java
+++ app/src/main/java/com/example/word21_notification/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.word21_notification;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this,NotificationActivity.class);
+                PendingIntent pi=PendingIntent.getActivity(MainActivity.this,0,intent,0);
                 notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 Notification notification=new NotificationCompat.Builder(MainActivity.this,"default")
                         .setContentTitle("这是标题")
@@ -28,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
                         .setWhen(System.currentTimeMillis())
                         .setSmallIcon(R.drawable.ic_launcher_background)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher_background))
+                        .setContentIntent(pi)
                         .build();
                 notificationManager.notify(1,notification);
             }
