915baaf23ad6727c11cb6707a52c4943967bd597
Your Name <you@example.com>
2018-07-22 15:56:50 +0800

    Notification通知

---
+++ app/src/main/java/com/example/word21_notification/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.word21_notification;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private NotificationManager notificationManager;
+    private Button send;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        send=(Button)findViewById(R.id.send_notice);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                notificationManager=(NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                Notification notification=new NotificationCompat.Builder(MainActivity.this,"default")
+                        .setContentTitle("这是标题")
+                        .setContentText("这是内容")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher_background))
+                        .build();
+                notificationManager.notify(1,notification);
+            }
+        });
+
+
+    }
+}
